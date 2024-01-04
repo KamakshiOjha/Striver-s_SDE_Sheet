7f99647833f2812d31767153c722a8f49bb8ab4e
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-01-04 22:33:55 +0530

    Pattern - 20

---
+++ Patter_20.java
@@ -0,0 +1,33 @@
+public class Patter_20 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<5){
+            System.out.print("\n");
+            for(int k = 5-i-1;k>0;k--){
+                System.out.print("*");
+            }
+            for(int j = 0;j<2*i+1;j++){
+                System.out.print(" ");
+            }
+            for(int m = 5-i-1;m>0;m--){
+                System.out.print("*");
+            }
+            i++;
+        }
+    
+    i = 0;
+    while(i<5){
+            for(int k = 0;k<i;k++){
+                System.out.print("*");
+            }
+            for(int j = 0;j<2*5 - (2*i +1);j++){
+                System.out.print(" ");
+            }
+            for(int m = 0;m<i;m++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+            i++;
+        }
+    }
+}
