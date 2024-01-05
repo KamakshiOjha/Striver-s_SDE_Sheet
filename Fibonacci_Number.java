4ebe20ec69211534e06fa57540080e8cd864b3ae
Kamakshi  Ojha <114620432+KamakshiOjha@users.noreply.github.com>
2024-01-05 23:58:38 +0530

    Update Fibonacci_Number.java

--- Fibonacci_Number.java
+++ Fibonacci_Number.java
@@ -1,26 +1,42 @@
-// leetcode - 509. Fibonacci Number
-import java.util.*;
+import java.util.Scanner;
+
 public class Fibonacci_Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number:");
         int n = sc.nextInt();
-        System.out.println(fib(n));
-    }
-    public static int fib(int n) {
         int a = 0;
         int b = 1;
-        int count = 2;
-        int c = 0;
-        if(n == 0 || n==1){
-            return n;
+        System.out.println(a);
+        System.out.println(b);
+        fib(n,a,b);
+    }
+    //without recursion:
+    // public static void fib(int n) {
+    //     int a = 0;
+    //     int b = 1;
+    //     int c = 0;
+    //     int count = 2;
+    //     System.out.println(a);
+    //     System.out.println(b);
+    //     while(count<=n){
+    //         c = a+b;
+    //         System.out.println(c);
+    //         a = b;
+    //         b = c;
+    //         count++;
+    //     }
+    // }
+    //with recursion:
+    public static void fib(int n,int a,int b) {
+        int c = a+b;
+        System.out.println(c);
+        a=b;
+        b=c;
+        n = n-1;
+        if(n>0){
+            fib(n, a, b);
         }
-        while(count<=n){
-            c = a+b;
-            a = b;
-            b = c;
-            count++;
-        }
-        return c;
+        
     }
 }
